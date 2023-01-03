4c5a7d9f71ca1a768f0417ea7433e3ab5b3853e6
asam237 <abbasalimokolo@gmail.com>
2023-01-03 19:49:21 +0100

    feat: update experiences

--- store/experiences.js
+++ store/experiences.js
@@ -1,37 +1,51 @@
 export const state = () => ({
   experiences: [
     {
-      title: 'Web Developer',
-      year: '2020',
+      title: 'Web Developer (intern)',
+      year: 'June 2020 - September 2020',
       subTitle: 'SOCAPRESCO',
       content: 'Design of the showcase site of the structure. ',
     },
     {
-      title: 'Mobile Developer',
-      year: '2021',
+      title: 'Mobile Developer (intern)',
+      year: 'June 2021 - November 2021',
       subTitle: 'SAHEL VETCHI TECHNOLOGIES',
       content:
         'Design and development of an application business management and monitoring. ',
     },
     {
       title: 'Software Developer',
-      year: '2021 - PRESENT',
+      year: 'September 2021 - PRESENT',
       subTitle: 'OSS CAMEROON',
       content: 'Working with OSS Cameroon as a software developer ',
       links: [
         {
-          name: "OSS CAMEROON - Website",
-          link: "https://github.com/osscameroon/osscameroon-website/"
+          name: 'OSS CAMEROON - Website',
+          link: 'https://github.com/osscameroon/osscameroon-website/',
         },
         {
-          name: "Onacut",
-          link: "https://github.com/osscameroon/onacut"
+          name: 'Onacut',
+          link: 'https://github.com/osscameroon/onacut',
         },
         {
-          name: "JOBSIKA",
-          link: "https://github.com/osscameroon/camerdevs"
+          name: 'JOBSIKA',
+          link: 'https://github.com/osscameroon/camerdevs',
         },
-      ]
+      ],
+    },
+    {
+      title: 'Mobile Developer',
+      year: 'March 2022 - July 2022',
+      subTitle: 'Lagence Digital',
+      content:
+        'Design and development of an a platform to control presence in a given place in an automatic way.',
+    },
+    {
+      title: 'Software Developer',
+      year: 'July 2022 - PRESENT',
+      subTitle: 'DIGITRADE',
+      content:
+        'Web & Mobile Developer',
     },
   ],
 })
